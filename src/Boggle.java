c4def87821303746e9e89ebc1e6d66ca229b040e
BT_1E10_03 <sample@bths.edu>
2022-03-14 10:40:58 -0400

    commit

--- src/Boggle.java
+++ src/Boggle.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
 public class Boggle {
+
+    private LetterGrid board;
+    public Boggle()
+    {
+        setup();
+    }
+
+    private void setup()
+    {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What size board? (3, 4, 5, or 6): ");
+        int boardSize = scanner.nextInt();
+
+        board = new LetterGrid(boardSize);
+
+        // draws the board as part of setup
+        board.drawBoard();
+    }
+
 }
