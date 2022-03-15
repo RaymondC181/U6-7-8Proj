8fdbe2936415f785791cc4ec658aed2190c784cd
BT_1E10_03 <sample@bths.edu>
2022-03-15 10:58:28 -0400

    commit

--- src/LetterGrid.java
+++ src/LetterGrid.java
@@ -15,24 +15,22 @@ public class LetterGrid {
         }
     }
 
-    public void drawBoard()
+    public static void drawBoard()
     {
-        for (int row = 0; row < spaces.length; row++) {
-            for (int col = 0; col < spaces[0].length; col++) {
-
-                char[][] table = new char [row][col];
-                for (int r = 0; r < row; r++){
-                    for (int c = 0; c < col; c++){
-                        int number = (int) (Math.random() * 26) + 65;  //just move this line
-                        table[r][c] = (char) number;
-                        System.out.print(table[r][c] + " ");
-                    }//inner for loop
-                    System.out.println();
-                }//outer for loop
-            }
-        }
-
+        int rows = 4;
+        int columns = 4;
+        char[][] table = new char [rows][columns];
+        for (int r = 0; r < rows; r++){
+            for (int c = 0; c < columns; c++){
+                int number = (int) (Math.random() * 26) + 65;  //just move this line
+                table[r][c] = (char) number;
+                System.out.print(table[r][c] + " ");
+            }//inner for loop
+            System.out.println();
+        }//outer for loop
     }
 
 }
+
+
 
--- src/Space.java
+++ src/Space.java
@@ -1,35 +1,17 @@
 public class Space
 {
     public static final String BLANK = " ";
-    static int MAX = 26;
     private String symbol;
 
     public Space()
     {
-        String rand = randAlp(10);
-        symbol = rand;
+        symbol = BLANK;
     }
 
     public String getSymbol()
     {
         return symbol;
     }
-
-
-    public String randAlp(int n) {
-        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                'h', 'i', 'j', 'k', 'l', 'm', 'n',
-                'o', 'p', 'q', 'r', 's', 't', 'u',
-                'v', 'w', 'x', 'y', 'z'};
-
-        String res = "";
-        for (int i = 0; i < n; i++)
-            res = res + alphabet[(int) (Math.random() * 10 % MAX)];
-
-        return res;
-    }
-
-
 
 
 
